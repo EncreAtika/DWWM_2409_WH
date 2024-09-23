d9b229b5ed5fa14349821fe04f4a0453856f2999
EncreAtika <166002828+EncreAtika@users.noreply.github.com>
2024-09-23 10:26:29 +0200

    creation projet TriDeNombe

---
+++ 01-Algorithmes/TriDeNombres/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TriDeNombres
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
