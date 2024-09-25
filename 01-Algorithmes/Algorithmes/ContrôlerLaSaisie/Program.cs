054ced4d75e522e58628ed2ba5b285cb399b97a8
EncreAtika <166002828+EncreAtika@users.noreply.github.com>
2024-09-25 14:53:16 +0200

    Fin Contrôler la Saisie

---
+++ 01-Algorithmes/Algorithmes/ContrôlerLaSaisie/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ContrôlerLaSaisie
+{
+    internal class Program
+    {
+        static void Main(string[] args)        
+        {
+
+            string prenom;
+
+           
+                Console.WriteLine("What's your name ?!");
+                prenom = Console.ReadLine();
+
+
+
+            while (prenom.Length < 2)
+            
+            { Console.WriteLine("Veuillez saisir un vrai prénom !");
+                prenom = Console.ReadLine();
+            }
+
+            Console.WriteLine("Bonjour " + prenom);
+
+        }
+    }
+}
