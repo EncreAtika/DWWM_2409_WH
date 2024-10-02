3e1303bdc4c4d7b514f9e881d8e92e67bcbc19d2
EncreAtika <166002828+EncreAtika@users.noreply.github.com>
2024-10-02 09:42:55 +0200

    Fin de Es tu Majeur

--- 01-Algorithmes/Algorithmes/EsTuMajeur/Program.cs
+++ 01-Algorithmes/Algorithmes/EsTuMajeur/Program.cs
@@ -4,7 +4,38 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // VARIABLE
+            int age;
+            string ?saisiUtilisateur; // En mettant le "?" juste avant le nom de la variable on le rend "nulable" ce qui fait que le Console.ReadLine() ne "râle" plus ...
+
+            // TRAITEMENT 
+            try
+            {
+                Console.WriteLine("Bonjour, veuillez saisir votre age : ");
+                saisiUtilisateur = Console.ReadLine();
+                age = int.Parse(saisiUtilisateur);
+
+                // AFFICHAGE
+                if (age >= 18)
+                {
+                    Console.WriteLine("Vous êtes majeur.");
+                }
+
+                else if (age < 0)
+                {
+                    Console.WriteLine("Vous n'êtes pas né.");
+                }
+
+                else 
+                {
+                    Console.WriteLine("Vous êtes mineurs.");
+                }
+                        } 
+            
+            catch(Exception ex) 
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
