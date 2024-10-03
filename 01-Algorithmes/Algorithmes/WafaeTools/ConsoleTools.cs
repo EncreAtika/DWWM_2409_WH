fbbc67afec4af77e936b804ec1d1770dc8021662
EncreAtika <166002828+EncreAtika@users.noreply.github.com>
2024-10-03 11:43:42 +0200

    Travail entre ConsoleTools et Tri de nombres

---
+++ 01-Algorithmes/Algorithmes/WafaeTools/ConsoleTools.cs
@@ -0,0 +1,37 @@
+﻿using System.Numerics;
+
+namespace WafaeTools
+{
+    public class ConsoleTools
+    {
+        public static int DemanderNombreEntier(string _question)
+        {
+            string saisieUtilisateur;
+
+            int valeurRetour;
+
+            bool saisieOK;
+
+            do
+            {
+                Console.WriteLine(_question);
+
+                saisieUtilisateur = Console.ReadLine() ?? "";
+
+                saisieOK = int.TryParse(saisieUtilisateur, out valeurRetour);
+
+                // valeurRetour = int.Parse(saisieUtilisateur);
+                // valeurRetour = Convert.ToInt32(saisieUtilisateur);
+    
+
+                if (!saisieOK) 
+                {
+                    Console.WriteLine("Saisie invalide, recommencez !");
+                }
+
+            } while (!saisieOK);
+
+            return valeurRetour;    
+        }
+    }
+}
