0abcbaa1b6c286d73ea519407c9ca369bfd6d6db
EncreAtika <166002828+EncreAtika@users.noreply.github.com>
2024-10-07 15:45:41 +0200

    FIN NombrePremier

---
+++ 01-Algorithmes/Algorithmes/NombrePremier/Program.cs
@@ -0,0 +1,49 @@
+﻿/* Lire un nombre N et déterminer s'il est un nombre premier.
+  Un nombre premier n'est divisible que par 1 et lui même */
+
+namespace NombrePremier
+{
+    internal class Program
+
+    {
+        static void Main(string[] args)
+        {
+           int nombreSaisi;
+           int mystere = 0;          
+
+            Console.WriteLine("Veuillez entrer un nombre : " );
+            nombreSaisi = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            if (nombreSaisi == 1)
+            {
+                Console.WriteLine(" Le nombre " + nombreSaisi + " n'est pas un nombre premier");
+            }
+
+            else if (nombreSaisi == 2) 
+            {
+                Console.WriteLine(" Le nombre " + nombreSaisi + " est un nombre premier");
+            }
+                 
+            for (int i = 2; i < nombreSaisi ; i++ )
+            {              
+                if (nombreSaisi % i == 0)
+                {
+                mystere++;                     
+                }           
+            }
+
+            if (mystere > 0) 
+            { 
+                Console.WriteLine(" Le nombre " + nombreSaisi + " n'est pas un nombre premier"); 
+            }
+
+            else 
+            {
+                Console.WriteLine(" Le nombre " + nombreSaisi + " est un nombre premier");
+            }
+        }
+    }
+}
+
+// Merci à Rémi pour ses explications
