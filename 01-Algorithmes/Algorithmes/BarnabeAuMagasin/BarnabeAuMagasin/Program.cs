b2dfd70e2770257e5243e449ceb295ca87b3e35a
EncreAtika <166002828+EncreAtika@users.noreply.github.com>
2024-10-14 15:40:33 +0200

    Barnabé au Magasin - à suivre-

---
+++ 01-Algorithmes/Algorithmes/BarnabeAuMagasin/BarnabeAuMagasin/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace BarnabeAuMagasin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Barnabé fait du Shopping!");
+
+            int BudgetBarnabe;
+            int magasin = 0;
+
+            do
+            {
+                magasin++;
+
+                do
+                {
+                    Console.WriteLine("Saissez la somme initiale que possède Barnabé pour son shopping : ");
+                    BudgetBarnabe = int.Parse(Console.ReadLine());
+
+                }
+                while ( magasin > 0 );
+                return;
+                
+            }
+        
+            while ( magasin > 0 );  
+            
+            }
+
+    }
+}
