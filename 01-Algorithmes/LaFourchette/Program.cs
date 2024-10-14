a67c1ba123f7f1b89a89aeb4133cdbd43324799f
EncreAtika <166002828+EncreAtika@users.noreply.github.com>
2024-10-14 08:57:59 +0200

    Fin LaFourchette

---
+++ 01-Algorithmes/LaFourchette/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace LaFourchette
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("La fourchette");
+
+            int nMystere = new Random().Next(0, 101);
+            int fMax = 100;
+            int fMin = 0;
+            int choixUtilisateur;
+            int nbEssais = 0;
+            bool gagne;
+            bool cOk;
+
+
+            do
+            {
+                nbEssais++;
+
+                do
+                {
+                    Console.WriteLine("Saisissez un nombre : ");
+                    cOk = int.TryParse(Console.ReadLine(), out choixUtilisateur);
+                    if (!cOk)
+                    {
+                        Console.WriteLine(" Ce n'est pas un nombre, veuillez recommencer !");
+                    }
+                }
+                while (!cOk);
+
+
+                if (choixUtilisateur < nMystere)
+                {
+                    fMin = choixUtilisateur;
+                    Console.WriteLine("Votre nombre est plus petit que le nombre mystère, le bon nombre se situe entre : " + fMin + " et " + fMax + " .");
+                    gagne = false;
+                }
+                else if (choixUtilisateur > nMystere)
+                {
+                    fMax = choixUtilisateur;
+                    Console.WriteLine($"Votre nombre est plus grand que le nombre mystère, le bon nombre se situe entre :{fMin} et {fMax} .");
+                    gagne = false;
+
+                }
+
+            }
+            while (choixUtilisateur != nMystere);
+
+            Console.WriteLine("Bravo, vous avez trouvé au bout de " + nbEssais + " essai !");
+
+
+        }
+    }
+}
+
+
+
