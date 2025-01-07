eecd3ab5d1d77987adb0f15e2041ba113c7ac46e
EncreAtika <166002828+EncreAtika@users.noreply.github.com>
2025-01-07 15:58:23 +0100

    TRavaux sql

--- SQL/rezo_social/rezo_requetes.sql
+++ SQL/rezo_social/rezo_requetes.sql
@@ -6,10 +6,16 @@ FROM utilisateur;
 /* 2-Sélectionner toutes les publications (titre, date, contenu, id utilisateur) triées par date de la plus récente à la plus ancienne */
 SELECT publi_titre, publi_date, publi_contenu, id
 FROM publication
-ORDER BY publi_date asc ;
+ORDER BY publi_date desc ;
+
+-- Sélectionner toutes les publications 
+-- triées par id utilisateur puis par date de la plus récente à la plus ancienne.
+EXPLAIN SELECT * FROM publication ORDER BY id ASC, publi_date DESC;
+-- Pour les requête complexe le EXPLAIN permet d'optimiser les requêtes et de savoir ce qui ne va pas.
 
 
-/*3-Sélectionner les publications (pub_id, date, titre) de l'utilisateur N°2.*/
+
+/*3-Sélectionner les publications de l'utilisateur 2 (pub_id, date, titre)*/
 SELECT (id=2), publi_id, publi_date, publi_titre
 FROM  publication
 WHERE id = 2;
@@ -21,6 +27,12 @@ FROM publication
 WHERE publi_titre LIKE "%a%"
 ORDER BY publi_titre DESC;
 
+/*4_bis -Sélectionner les publications (pub_id, titre, contenu) dont le titre ne contient pas la lettre "a". Le résultat est trié par le titre et par ordre décroissant.*/
+SELECT publi_id, publi_titre, publi_contenu
+FROM publication
+WHERE publi_titre NOT LIKE "%a%"
+ORDER BY publi_titre DESC;
+
 
 /*5-Sélectionner les utilisateurs (id, nom, email) dont l'adresse email se termine par "com".*/
 SELECT id, utilisateur_nom, utilisateur_email
@@ -28,5 +40,17 @@ FROM utilisateur
 WHERE utilisateur_email LIKE "%.com%";
 
 /*6-Sélectionner les publications triées par titre (ordre alphabétique) avec le nom d'utilisateur de l'auteur (nécessite une jointure)*/
+SELECT * FROM publication
+INNER JOIN utilisateur ON publication.id = utilisateur.id
+ORDER BY publication.publi_titre;
+
+/*6 bis -Selectionner les publications triées par titre (ordre alphabétique)
+avec le nom d'utilisatuer de l'auteur */
+SELECT * FROM publication, utilisateur
+WHERE publication.id = utilisateur.id;
+
+
+/*Je souhaite consulter ma table utilisateur pour voir si une ligne avec l'ID 1 existe déjà*/
+SELECT * FROM utilisateur WHERE id = 1;
 
 
--- SQL/rezo_social/rezo_social_bdd.sql
+++ SQL/rezo_social/rezo_social_bdd.sql
@@ -5,27 +5,39 @@ USE rezo_social;
 CREATE TABLE utilisateur
 (
 	id INT PRIMARY KEY,
-	utilisateur_nom VARCHAR(32),
-	utilisateur_email VARCHAR(128)
+	utilisateur_nom VARCHAR(32) NOT NULL UNIQUE,
+	utilisateur_email VARCHAR(128) UNIQUE NOT NULL
 );
+
+CREATE TABLE aimer
+(
+	id INT,
+    publi_id INT,
+    PRIMARY KEY (id, publi_id)
+  );
 
 CREATE TABLE publication
 (
 	publi_id INT AUTO_INCREMENT PRIMARY KEY,
-    publi_date DATETIME,
+    publi_date DATETIME NOT NULL,
     publi_titre VARCHAR(255),
     publi_contenu TEXT,
-    id INT,
-    FOREIGN KEY (id) REFERENCES utilisateur(id)
-);
+    id INT
+	);
+    
+/*Modifier la table publication et y ajouter une clé étrangère*/
+ALTER TABLE publication ADD FOREIGN KEY (id) REFERENCES utilisateur(id);
 
-CREATE TABLE aime
-(
-	id INT,
-    publi_id INT,
-    PRIMARY KEY (id, publi_id),
-    FOREIGN KEY (id) REFERENCES utilisateur(id),
-	FOREIGN KEY (publi_id) REFERENCES publication(publi_id)
-);
+ALTER TABLE aimer ADD CONSTRAINT fk_aimer_utilisateur FOREIGN KEY (id) REFERENCES utilisateur (id);
+
+ALTER TABLE aimer ADD CONSTRAINT fk_aimer_publication FOREIGN KEY (publi_id) REFERENCES publication (publi_id);
+
+/*ALTER TABLE aimer ADD CONSTRAINT fk_aimer_utilisateur FOREIGN KEY (id) REFERENCES utilisateur (id),
+					ADD CONSTRAINT fk_aimer_publication FOREIGN KEY (publi_id) REFERENCES publication (publi_id);*/	
+
+/*ALTER TABLE aimer DROP CONSTRAINT fk_aimer_utilisateur;*/
+
+
+
 
 
