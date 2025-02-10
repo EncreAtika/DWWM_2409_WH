98f5f2de735d9f975b8493c2c079e857bd084914
EncreAtika <166002828+EncreAtika@users.noreply.github.com>
2025-02-10 15:49:31 +0100

    Poo travail sur les bouteilles

---
+++ poo/Poo/Poo/Program.cs
@@ -0,0 +1,33 @@
+﻿using ClassLibraryBouteille;
+using System.Runtime.CompilerServices;
+
+namespace Poo
+{
+    internal class Program
+    {
+
+        static void Main(string[] args) 
+        { 
+            Bouteille bouteille1 = new Bouteille();
+            Bouteille bouteilleOuverte = new Bouteille(1.5f, 0.75f, true);
+            Bouteille bouteilleClonee = new Bouteille(bouteille1);
+            Bouteille bouteilleVin = new Bouteille(1.25f, 0.5f, false);
+            Bouteille bouteilleHuile = new Bouteille(0.75f, 0.75f, true);
+
+            Console.WriteLine(bouteilleOuverte.contenuEnLitre + " " + bouteilleOuverte.estOuverte );
+            Console.WriteLine();
+            Console.WriteLine(bouteille1.estOuverte + " " + bouteilleClonee.estOuverte);
+            Console.WriteLine();
+            Console.WriteLine(bouteilleVin.contenuEnLitre  + " " + bouteilleClonee.estOuverte );
+            Console.WriteLine();
+            Console.WriteLine(bouteilleHuile.contenanceEnLitre + " " + bouteilleVin.estOuverte);
+;        }
+    
+    
+    
+    
+    
+    
+    }
+}
+  
