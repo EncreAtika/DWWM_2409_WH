11af8d9a6416e5c5b44951a5c3e91e9f3689425e
EncreAtika <166002828+EncreAtika@users.noreply.github.com>
2025-02-25 09:56:26 +0100

    exercice taille texte presque terminé

--- WebFront/Json_Javascript/Introduction Javascript 2/assets/script.js
+++ WebFront/Json_Javascript/Introduction Javascript 2/assets/script.js
@@ -1,17 +1,32 @@
 let taille = 16;
 
 const addWeight = document.querySelector(".add-weight");
+const lessWeight = document.querySelector(".less-weight");
 const tailleActuelle = document.querySelector("#taille-actuelle");
 const texte = document.querySelector("#texte");
 
 tailleActuelle.innerHTML = taille;
+
 addWeight.addEventListener("click", () => {
+  if (taille < 48) {
     taille++;
-    if(taille < 48) {
-        tailleActuelle.innerHTML++;
-    }
-    else {
-        tailleActuelle.innerHTML = 16;
-    }
-    texte.style.fontSize = tailleActuelle.innerHTML + "px";
+    tailleActuelle.innerHTML = taille;
+    texte.style.fontSize = taille + "px";
+  } else {
+    taille = 16;
+    tailleActuelle.innerHTML = taille;
+    texte.style.fontSize = taille + "px";
+  }
+});
+
+lessWeight.addEventListener("click", () => {
+  if (taille > 8) {
+    taille--;
+    tailleActuelle.innerHTML = taille;
+    texte.style.fontSize = taille + "px";
+  } else {
+    taille = 16;
+    tailleActuelle.innerHTML = taille;
+    texte.style.fontSize = taille + "px";
+  }
 });
