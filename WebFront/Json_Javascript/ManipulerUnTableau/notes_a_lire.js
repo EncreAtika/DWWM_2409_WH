be6eb242538087e4fda7280297cfeeae1370d421
EncreAtika <166002828+EncreAtika@users.noreply.github.com>
2025-03-05 15:58:27 +0100

    exercicce Manipuler un tableau en cours

---
+++ WebFront/Json_Javascript/ManipulerUnTableau/notes_a_lire.js
@@ -0,0 +1,52 @@
+
+
+
+/*const people = ['Mike Dev', 'John Makenzie', 'Léa Grande']; 
+const laListe = document.querySelector("#laListe");
+const liste = document.querySelector("#liste");
+const filet = document.querySelector("#cadre");
+
+
+const trHead = document.createElement("tr")
+        const _nom = document.createElement("th")
+        const _prenom = document.createElement("th")
+        const _eMail = document.createElement("th")*/
+
+/*liste.setAttribute("style", "list-style:none; color:#ACCBE1");
+
+document.body.style.backgroundColor = "#536B78";
+document.body.style.color = "#ACCBE1";
+document
+  .querySelector("h1")
+  .setAttribute(
+    "style",
+    "margin-left:50px; font-family:tahoma;text-transform:uppercase"
+  );
+
+document
+  .querySelector("#filet")
+  .setAttribute("style", "border:2px dashed #ACCBE1");
+
+for (let i = 0; i < people.length; i++) {
+  let monItem = document.createElement("li");
+  monItem.setAttribute("style", "font-family:tahoma; font-size:1.6rem");
+  monItem.textContent = people[i];
+  liste.appendChild(monItem);
+}
+  */
+
+//##############################################################################//
+//                                                                              //
+// TABLEAU SIMPLE : const people = ["Mike Dev", "John Makenzie", "Léa Grande"]; //
+//                                                                              //
+//##############################################################################//
+
+
+//##############################################################
+//                                                            //
+// - people. push() - ajoute un élèment à la fin du tableau   //
+// - people.unshift() - ajoute un élèment au début du tableau.//
+// - people.pop() - supprime le dernier élèment du tableau    //
+// - people.shift()- pour supprimer le début du tableau.      //
+//                                                            //
+//############################################################//
