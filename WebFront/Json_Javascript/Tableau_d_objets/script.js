6ed3941d5386bffaa05286751ccd2a9e312266f3
EncreAtika <wafae.hammouchi@hotmail.com>
2025-03-21 18:51:11 +0100

    exercice tableau d'objets en cours

--- WebFront/Json_Javascript/Tableau_d_objets/script.js
+++ WebFront/Json_Javascript/Tableau_d_objets/script.js
@@ -1,2 +1,10 @@
-const theForm = document.querySelector("the-form");
-cont
+const boxForm = document.querySelector("#box-form");
+const form = document.querySelector("form");
+const inputId = document.querySelector("#input-id");
+const inputPassword = document.querySelector("#input-password");
+const btnConnexion = document.querySelector("#btn-connexion");
+const formError = document.querySelector("#form-error");
+const table = document.querySelector("table");
+const tBody = document.querySelector("tbody");
+
+
